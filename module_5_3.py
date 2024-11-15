961e2e0a5edfe96387cc503d4cadc08a920d0378
AlexRudt <Aleksrudt@yandex.ru>
2024-11-15 15:26:26 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -9,6 +9,9 @@ class House:
         else:
             print('Такого этажа не существует')
 
+    def __len__(self):
+        return self.new_floor
+
     def __str__(self):
         return f'Название:{self.name}, кол-во этажей: {self.number_of_floors}'
 
@@ -44,30 +47,31 @@ class House:
             self.number_of_floors += value
             return self
 
+def main():
+    h1 = House('ЖК Эльбрус', 10)
+    h2 = House('ЖК Акация', 20)
 
 
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
+    print(h1)
+    print(h2)
 
-print(h1)
-print(h2)
+    print(h1 == h2) # __eq__
 
-print(h1 == h2) # __eq__
+    h1 = h1 + 10 # __add__
+    print(h1)
+    print(h1 == h2)
 
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
+    h1 += 10 # __iadd__
+    print(h1)
 
-h1 += 10 # __iadd__
-print(h1)
+    h2 = 10 + h2 # __radd__
+    print(h2)
 
-h2 = 10 + h2 # __radd__
-print(h2)
+    print(h1 > h2) # __gt__
+    print(h1 >= h2) # __ge__
+    print(h1 < h2) # __lt__
+    print(h1 <= h2) # __le__
+    print(h1 != h2) # __ne__
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
-
-
+if __name__ == '__main__':
+    main()
